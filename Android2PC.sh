b5520027e792cca5449d19cfe8d3da52befc5d56
Allgamesdude <kedz07@gmail.com>
2016-02-18 15:33:21 -0800

    Update Android2PC.sh

--- Android2PC.sh
+++ Android2PC.sh
@@ -4,33 +4,36 @@
 			echo Ready to use ADB commands.
 			sleep
 			echo
-			echo Showing currently connected ADB devices. Serial number first, then status.
+			echo Showing currently connected ADB devices. Serial number first, status.
 			sleep
 			adb devices
 			echo
 			adb get-serialno
-			!serialno!
-			echo !SERIALNO!
-			adb pull !inputDir! !outputDir!
+			$serialno
+			echo "$serialno"
+			adb pull "$inputDir" "$outputDir"
 			sleep
 			echo
-			echo Pulled from the input Directory !inputDir! and pushed to the output Directory !outputDir!.
-		}
+			echo Pulled from the input Directory "$inputDir" and pushed to the output Directory "$outputDir".
+					}
 	echo Start Android2PC program
 	echo i INPUT
 	echo o OUTPUT
 	echo a ADB
-	if "$~1"=="-i" set inputDir=$2 
-	if "$~3"=="-o" set outputDir=$4 
-	if "$~5"=="-a" ADB 
-	SET inputDir=$2
-	SET outputDir=$4
-	echo New input directory is !inputDir!
-	sleep  
-	mkdir !outputDir!
-	mkdir !outputDir!\output      
-	echo New output directory is !outputDir!
-	sleep
-	echo
-	sleep
-	echo Program complete, exit command line.
+	if [ "$1" = "-i" ]; then
+	 set "inputDir" = "$2" 
+	fi
+	if [ "$3" = "-o" ]; then
+	 set "outputDir" = "$4" 
+	fi
+	if [ "$5" = "-a" ]; then
+	 ADB 
+	fi
+	set "inputDir" = "$2"
+	set "outputDir" = "$4"
+	echo New input directory is $inputDir
+	mkdir -p "$outputDir"
+	mkdir -p "$outputDir"\\output      
+	echo New output directory is $outputDir
+	echo Program complete, exit command line
+	exit 0
